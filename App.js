b32fd7490e958c8fd1944dfe6f3f4ce3e763c18c
Minhye Kang <alsgp9898@naver.com>
2022-07-05 23:45:34 +0900

    git branch test

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, Text } from "@react-navigation/native";
 import Login from "./components/Login";
 import Stack from "./navigation/Stack";
 // tabBarStyle: { display: 'none' }
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <>
       <Login />
+      <Text>민혜</Text>
       <NavigationContainer>
         <Stack />
       </NavigationContainer>
