03c635f712cf34d98bb88c077bdc011d03063d67
Minhye Kang <alsgp9898@naver.com>
2022-07-06 15:41:13 +0900

    Style: App.js conflic resolve

--- App.js
+++ App.js
@@ -1,30 +1,10 @@
 import React, { useState } from "react";
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-import { NavigationContainer, Text } from "@react-navigation/native";
-import Login from "./components/Login";
-=======
-import { NavigationContainer } from "@react-navigation/native";
->>>>>>> Stashed changes
-import Stack from "./navigation/Stack";
-
-export default function App() {
-  return (
-    <>
-<<<<<<< Updated upstream
-      <Login />
-      <Text>민혜</Text>
-=======
 import { NavigationContainer } from "@react-navigation/native";
 import Root from "./navigation/Root";
 
 export default function App() {
   return (
     <>
->>>>>>> 359bac7f4a3e2d97ad53bfb82a6a76f832695636
-=======
-      {/* <Login /> */}
->>>>>>> Stashed changes
       <NavigationContainer>
         <Root />
       </NavigationContainer>
