5f1e95fe891ac703fa8b2c104a29e48326d709d6
Cheol-Cheol <dream174822@gmail.com>
2022-07-06 14:27:51 +0900

    ✨ Feat: Add Tab + Stack navigation

---
+++ screens/Chat.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Chat = () => {
+  return <Text>Chat 페이지</Text>;
+};
+
+export default Chat;
---
+++ screens/User.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const User = () => {
+  return <Text>User 페이지</Text>;
+};
+
+export default User;
