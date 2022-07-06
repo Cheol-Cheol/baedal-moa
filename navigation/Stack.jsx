371b78902b74299e5f70bf39f122ce8c07de4ad6
Cheol-Cheol <dream174822@gmail.com>
2022-07-06 11:36:31 +0900

    ✨ Feat: Add Login stack-navigation

--- navigation/Stack.jsx
+++ navigation/Stack.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SignUp from "../screens/SingUp";
+import SignUp from "../screens/SignUp";
 import FindUser from "../screens/FindUser";
+import Login from "../screens/Login";
 
 const NativeStack = createNativeStackNavigator();
 
 const Stack = () => (
-  <NativeStack.Navigator>
-    <NativeStack.Screen name="SignUp" component={SignUp} />
-    <NativeStack.Screen name="FindUser" component={FindUser} />
+  <NativeStack.Navigator
+    initialRouteName="Login"
+    screenOptions={{ headerBackTitleVisible: false }}
+  >
+    <NativeStack.Screen
+      name="Login"
+      component={Login}
+      options={{ headerShown: false }}
+    />
+    <NativeStack.Screen name="회원가입" component={SignUp} />
+    <NativeStack.Screen name="계정 찾기" component={FindUser} />
   </NativeStack.Navigator>
 );
 
--- screens/SignUp.jsx
+++ screens/SignUp.jsx
@@ -3,7 +3,7 @@ import { Text, TouchableOpacity } from "react-native";
 
 const SignUp = ({ navigation: { goBack } }) => (
   <>
-    <Text>계정 찾기 페이지</Text>
+    <Text>회원가입 페이지</Text>
     <TouchableOpacity onPress={() => goBack()}>
       <Text>로그인 하러가기</Text>
     </TouchableOpacity>
